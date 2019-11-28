8c034e77f9a67cdd31f4c215dc7c19aa70d74b5e
Dvyd <dvyd98@gmail.com>
2019-11-28 12:37:38 +0100

    no message

--- src/main/java/com/aswg12c/demo/AswApplication.java
+++ src/main/java/com/aswg12c/demo/AswApplication.java
@@ -2,10 +2,12 @@ package com.aswg12c.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication
+@EnableOAuth2Sso
 public class AswApplication {
 
 	public static void main(String[] args) {
